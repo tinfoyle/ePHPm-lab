6ad46f2953a4fc026bf546fef99cb7310be15d2c
Luther Monson <luther.monson@gmail.com>
2026-07-10 06:44:17 -0700

    Pin wordpress-worker 0.1.1 and note the upstream lifecycle fix

--- wordpress-v5/scripts/install-wordpress-worker.sh
+++ wordpress-v5/scripts/install-wordpress-worker.sh
@@ -12,4 +12,4 @@ composer config --global github-protocols https
 composer init --name=ephpm-lab/wordpress-v5 --no-interaction
 composer config repositories.wordpress-worker vcs https://github.com/ephpm/wordpress-worker
 composer config repositories.php-worker vcs https://github.com/ephpm/php-worker
-composer require --no-interaction --prefer-dist ephpm/wordpress-worker:^0.1
+composer require --no-interaction --prefer-dist ephpm/wordpress-worker:0.1.1
